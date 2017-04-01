7400ce36354932af78238f4f919330f265e7aa14
Nickrosendo <nicolasrosendo@hotmail.com>
2017-04-01 20:22:35 -0300

    Passando arquivos para o repo

---
+++ js/carrega_html.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#modal').load('modal.html');
+	$('#deslogado').load('navbar_deslogado.html');
+	$('#logado').load('navbar_logado.php');
+	$('#rodape').load('rodape.html');
+});
